ddd3f448f6e017e0323abe7684b537f5d268d173
Harold Barleta <hbf20c@gmail.com>
2023-02-09 23:56:23 -0800

    added countdown and viewmodelfun assignments

---
+++ ASPdotNetCore/ASPMVCII/ViewModelFun/Models/Strings.cs
@@ -0,0 +1,4 @@
+public class Strings
+{
+    string messsage = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia accusantium a consectetur vitae molestiae sequi nam incidunt, corporis nihil labore reprehenderit ex provident porro, voluptatem ad sapiente ratione suscipit. Expedita.";
+}
